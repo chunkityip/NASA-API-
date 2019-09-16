c5ce4c7d4c222da361f062535fb7a2afbfd128c8
Chun Kit Yip <47329780+chunkityip@users.noreply.github.com>
2019-09-16 12:18:20 -0400

    Add files via upload

---
+++ API/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for
+import requests
+from flask_fontawesome import FontAwesome
+#4 libraries, flask, requests, flask_fontawesome and fontawesome
+
+app = Flask(__name__)
+fa = FontAwesome(app)
+
+url = requests.get(
+    'https://api.nasa.gov/planetary/apod?api_key=iBeT2hXG8tezilCqlJ5IXkNpTPWBFpB95bo72qyh')
+text = url.json()
+image = text['url']
+name = text['copyright']
+dt = text['date']
+ti = text['title']
+expl = text['explanation']
+
+info = [{'title': ti,
+         'name': name,
+         'day': dt,
+         'detail': expl,
+         'image': image}]
+
+#using dummy data to jinja2
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html', posts=info)
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def lionel():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
